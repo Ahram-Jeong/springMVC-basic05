d2e7403080a07839507c7abe239575e41f8c66f6
Ahram-Jeong <ahram1215@gmail.com>
2024-01-09 18:29:12 +0900

    20240109 - Bean Validation_@ScriptAssert

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 계가 10,000원이 넘도록 입력해 주세요.")
+// 실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우도 있기 때문에 @ScriptAssert는 기능이 약해서 권장하지 않음
 public class Item {
 
     private Long id;
